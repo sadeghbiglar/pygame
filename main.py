57a9123ff48695e8693c1aaabef899abbb80f4db
sadeghbiglar <sbmail555@gmail.com>
2025-01-26 23:36:41 +0330

    create first main window and draw , text

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+print("running")
+
+screen=pygame.display.set_mode((800,600))
+pygame.display.set_caption("myGame...")
+running=True
+blue=(0,0,255)
+while running:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+        screen.fill((255,255,0))
+        pygame.draw.circle(screen,blue,(500,400),50)
+        font=pygame.font.Font(None,50)
+        text=font.render("step1",True,(0,0,0))
+        screen.blit(text,(100,100))
+        pygame.display.flip()
+pygame.quit()
