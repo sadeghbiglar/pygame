d0b5ec72d42b9e0c92c069997d00253e1d962d87
sadeghbiglar <sbmail555@gmail.com>
2025-01-29 00:29:38 +0330

    add keys event

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ screen=pygame.display.set_mode((800,600))
 pygame.display.set_caption("myGame...")
 running=True
 blue=(0,0,255)
+x=100
+y=390
 while running:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
@@ -23,7 +25,16 @@ while running:
 
         player=pygame.image.load('player.png')
         player=pygame.transform.scale(player,(100,125))
-        screen.blit(player,(100,390))
+        screen.blit(player,(x,y))
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_UP:
+               y=y-10
+            elif event.key==pygame.K_DOWN:
+                y=y+10
+            elif event.key==pygame.K_LEFT:
+                x=x-10
+            elif event.key==pygame.K_RIGHT:
+                x=x+10
         if event.type==pygame.MOUSEBUTTONDOWN:
                     pygame.draw.line(screen,(0,255,0),(0,517),(1000,517),6)
 
